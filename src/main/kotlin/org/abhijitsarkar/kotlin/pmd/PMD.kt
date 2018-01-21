2c1f855f2b98a87b5dd8460b74c9b5685b8ce09a
Abhijit Sarkar <asarkar@abhijitsarkar.org>
2018-01-21 10:34:26 -0800

    Fixes https://github.com/pmd/pmd/issues/866

--- src/main/kotlin/org/abhijitsarkar/kotlin/pmd/PMD.kt
+++ src/main/kotlin/org/abhijitsarkar/kotlin/pmd/PMD.kt
@@ -1,9 +1,10 @@
 package org.abhijitsarkar.kotlin.pmd
 
-import com.github.kittinunf.fuel.core.FuelManager
-import com.github.kittinunf.fuel.httpGet
 import net.sourceforge.pmd.Ruleset
-import java.io.ByteArrayInputStream
+import org.slf4j.LoggerFactory
+import java.io.IOException
+import java.io.StringReader
+import java.net.HttpURLConnection
 import java.net.URL
 import java.util.Properties
 import java.util.concurrent.ConcurrentHashMap
@@ -14,30 +15,47 @@ import javax.xml.bind.JAXBContext
  */
 object PMD {
     private const val EMPTY = ""
+    private val LOGGER = LoggerFactory.getLogger(PMD::class.java)
+    private const val BASE_URL = "https://raw.githubusercontent.com/pmd/pmd/master/pmd-java/src/main/resources"
     private val JAXB_CTX = JAXBContext.newInstance(Ruleset::class.java)
 
     private val rulesets: MutableMap<String, Ruleset> = ConcurrentHashMap()
 
-    init {
-        FuelManager.instance.basePath = "https://raw.githubusercontent.com/pmd/pmd/master/pmd-java/src/main/resources"
-    }
-
     private fun categoriesInternal(): List<String> {
-        val (_, _, result) = "/category/java/categories.properties".httpGet().response()
-        val (data, error) = result
-        return if (error == null) {
-            val props = Properties().apply {
-                load(ByteArrayInputStream(data))
-            }
-            props["rulesets.filenames"]
-                    .toString()
-                    .replace("""\""", EMPTY)
-                    .replace("""\R""".toRegex(), EMPTY)
-                    .replace("""\s""".toRegex(), EMPTY)
-                    .split(",")
-        } else {
-            throw error
+        return (URL("$BASE_URL/category/java/categories.properties").openConnection() as HttpURLConnection).apply {
+            requestMethod = "GET"
+            doOutput = false
+            connectTimeout = 500
+            readTimeout = 3000
+            instanceFollowRedirects = true
+
+            connect()
         }
+                .let {
+                    when (it.responseCode) {
+                        in 200..399 -> {
+                            val props = it.inputStream.bufferedReader().use { it.readText() }
+                                    .let {
+                                        Properties().apply {
+                                            load(StringReader(it))
+                                        }
+                                    }
+                            props["rulesets.filenames"]
+                                    .toString()
+                                    .replace("""\""", EMPTY)
+                                    .replace("""\R""".toRegex(), EMPTY)
+                                    .replace("""\s""".toRegex(), EMPTY)
+                                    .split(",")
+                        }
+                        else -> {
+                            it.errorStream?.bufferedReader()?.use { it.readText() }
+                                    ?.run {
+                                        LOGGER.error(this)
+                                    }
+                            throw IOException(it.responseMessage)
+                        }
+                    }
+                }
     }
 
     fun categories(): Map<String, Ruleset> {
@@ -50,7 +68,7 @@ object PMD {
         return rulesets.computeIfAbsent(name, {
             JAXB_CTX
                     .createUnmarshaller()
-                    .unmarshal(URL("${FuelManager.instance.basePath}/$it"))
+                    .unmarshal(URL("$BASE_URL/$it"))
                     .let { it as Ruleset }
         })
     }
